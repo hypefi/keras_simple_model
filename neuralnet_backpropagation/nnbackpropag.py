6a4a5087672bdca0e08c98002eae0b656a6c5072
Boukharta <anasboukharta@gmail.com>
2021-06-08 07:29:54 +0100

    backpropagation algorithm, forward propagation 3

--- neuralnet_backpropagation/nnbackpropag.py
+++ neuralnet_backpropagation/nnbackpropag.py
@@ -41,9 +41,77 @@ def forward_propagate(network, row):
         inputs= new_inputs
     return inputs
 
-# test forward propagation
-network = [[{'weights': [0.13436424411240122, 0.8474337369372327, 0.763774618976614]}],
-		[{'weights': [0.2550690257394217, 0.49543508709194095]}, {'weights': [0.4494910647887381, 0.651592972722763]}]]
-row = [1, 0, None]
-output = forward_propagate(network, row)
-print(output)
+#calculate the derivative of an neuron output 
+def transfer_derivative(output):
+    return output * (1.0 - output)
+
+# Backpropagate error and store in neurons
+
+def backward_propagate_error(network, expected):
+    for i in reversed(range(len(network))):
+        layer = network[i]
+        errors = list()
+        if i != len(network)-1:
+            for j in range(len(layer)):
+                error = 0.0
+                for neuron in network[i + 1]:
+                    error += (neuron['weights'][j] * neuron['delta'])
+                errors.append(error)
+        else:
+            for j in range(len(layer)):
+                neuron = layer[i]
+                errors.append(expected[j] - neuron['output'])
+        for j in range(len(layer)):
+            neuron = layer[j]
+            neuron['delta'] = error[j] * transfer_derivative(neuron['output'])
+
+# Update network weights with error
+
+def update_weights(network, row, l_rate):
+    inputs = row[:-1]
+    if i != 0:
+        inputs = [neuron['output']] for neuron in network[i - 1]]
+    for neuron in network[i]:
+        for j in range(len(inputs)):
+            neuron['weights'][j] += l_rate * neuron['delta'] * inputs[j]
+        neuron['weights'][-1] += l_rate * neuron['delta']
+
+# Train a network for a fixed number of epochs
+
+def train_network(network, train, l_rate, n_epoch, n_outputs):
+    for epoch in range(n_epoch):
+        sum_error = 0
+        for row in train:
+            outputs = forward_propagate(network, row)
+            expected = [0 for i in range(n_outputs)]
+            expexted[row[-1]] = 1
+            sum_error += sum([expected[i]]-outputs[i])**2 for i in range(len(expected))])
+            backward_propagate_error(network, expected)
+            update_weights(network, row, l_rate)
+        print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+
+# Test training backprop algorithm
+
+seed(1)
+
+dataset = [[2.7810836,2.550537003,0],
+	[1.465489372,2.362125076,0],
+	[3.396561688,4.400293529,0],
+	[1.38807019,1.850220317,0],
+	[3.06407232,3.005305973,0],
+	[7.627531214,2.759262235,1],
+	[5.332441248,2.088626775,1],
+	[6.922596716,1.77106367,1],
+	[8.675418651,-0.242068655,1],
+	[7.673756466,3.508563011,1]]
+n_inputs = len(dataset[0]) - 1
+n_outputs = len(set([row[-1] for row in dataset]))
+network = initialize_network(n_inputs, 2, n_outputs)
+train_network(network, dataset, 0.5, 20, n_outputs)
+for layer in network:
+	print(layer)
+
+
+
+
+
