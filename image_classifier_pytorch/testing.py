2f7948c69a4e436c1908f7a715d2ae8aea737e8b
Boukharta <anasboukharta@gmail.com>
2021-07-02 09:46:00 +0100

    testing

--- image_classifier_pytorch/testing.py
+++ image_classifier_pytorch/testing.py
@@ -1,9 +1,20 @@
 import model
+import network
 
 dataiter = iter(model.testloader)
 images, labels = dataiter.next()
-
+PATH = './cifar_net.pth'
 #print images 
 
 model.imshow(model.torchvision.utils.make_grid(images))
-print('Groundtruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+print('Groundtruth: ', ' '.join('%5s' % model.classes[labels[j]] for j in range(4)))
+
+net = network.Net()
+net.load_state_dict(model.torch.load(PATH))
+
+outputs = net(images)
+
+_, predicted = model.torch.max(outputs, 1)
+
+print('Predicted: ', ' '.join('%5s' % model.classes[predicted[j]] for j in range(4)))
+
