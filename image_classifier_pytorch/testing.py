adf38b3ee52e01efb2bbc0b18f966a33c065b5e3
Boukharta <anasboukharta@gmail.com>
2021-07-02 09:56:36 +0100

    network accuracy

--- image_classifier_pytorch/testing.py
+++ image_classifier_pytorch/testing.py
@@ -18,3 +18,19 @@ _, predicted = model.torch.max(outputs, 1)
 
 print('Predicted: ', ' '.join('%5s' % model.classes[predicted[j]] for j in range(4)))
 
+correct = 0
+total = 0
+
+#since we're not training, we don't need to calculate the gradient for our outputs
+
+with model.torch.no_grad():
+    for data in model.testloader:
+        images, labels = data
+        # calculate outputs by running images through the network 
+        outputs = net(images)
+        #the class with the highest energy is what we choose as prediction 
+        _, predicted = model.torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
